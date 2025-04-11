42a98c06c0e8feeda8968584f39902245491fab6
Bax Picker <baxpicker@gmail.com>
2025-04-11 18:50:28 +0200

    ...

--- platformClimber/cfg/build_config.mk
+++ platformClimber/cfg/build_config.mk
@@ -32,7 +32,7 @@
 ##   change folder structure, change CPCT_PATH value for its absolute path.
 ##
 THIS_FILE_PATH := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-CPCT_PATH      := ${CPCT_PATH}
+CPCT_PATH      := /build/retro/projects/mytools/cpctelera-linux-cpc
 
 ####
 ## SECTION 1: Project configuration 
