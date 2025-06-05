63c7b6bed48fd7ab246f14f9347b88edf8a2aa5f
Bax Picker <baxpicker@gmail.com>
2025-06-05 12:54:24 +0200

    Update search for main entry point

--- docker/build_cpctelera_project_from_container.sh
+++ docker/build_cpctelera_project_from_container.sh
@@ -11,8 +11,11 @@ DEPLOY_EXTRA=${BUILD_DEPLOY_EXTRA}
 [[ "${DEPLOY_EXTRA}" != "true" ]] && [[ "${DEPLOY_EXTRA}" != "false" ]] && { echo "Error: BUILD_DEPLOY_EXTRA is not set."; exit 1; }
 
 # const variables
-RST_MAIN="${FOLDER_PROJECTS}/obj/main.rst"
+RST_MAIN="$(grep -r ' _main::' ${FOLDER_PROJECTS}/obj |grep .rst |cut -d':' -f1)"
+[[ "${RST_MAIN}" == "" ]] && { echo "Error: main entry point not found"; exit 1; }
+
 BUILD_CFG="${FOLDER_PROJECTS}/cfg/build_config.mk"
+[[ ! -f "${BUILD_CFG}" == "" ]] && { echo "Error: Build configuration file not found."; exit 1; }
 
 # optional variables (default empty/not used)
 PROJNAME=${BUILDCFG_PROJNAME:-""}
